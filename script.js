f1eed7dd03fc26fcca53c10689aa3e9d4c6943a3
DCoder117 <dhirajopjadhav17@gmail.com>
2026-08-26 15:27:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const getSum = () => {
 
     let row = document.createElement("tr");
     let cell = document.createElement("td");
-
+	cell.colSpan = 2;
     cell.textContent = "Total sum = " + sum;
 
     row.appendChild(cell);
